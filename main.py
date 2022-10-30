8930a14981059e45c42dd869f6611bdbacfbd976
PetrMakeev <109295765+PetrMakeev@users.noreply.github.com>
2022-10-30 16:36:23 +0500

    Определил класс координат

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from coordinates import get_gps_coordinates
+#from weather_api_service import get_wheather
+#from weather_formatter import format_weather
+
+
 # основное тело программы   
 def main():
-    
-    print('Hello World!')
+    coordinates = get_gps_coordinates()    
+    # wheather = get_wheather()
+
+    print(coordinates.latitude)
+
 
 # точка входа.
 if __name__ == '__main__':
